1a08a3b9058c3ad075fc85d2cc32fc8ffdadc73f
ANGELA MARTINEZ <angelaestmartinez@gmai.com>
2023-05-04 13:28:24 -0400

    1:28PM TESTING!

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Learn to design and code React app</h1>
     <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-    <p>Testing for git commit 1:24pm</p>
+    <p>Testing for git commit 1:28pm</p>
     <Link to="/page-2/">Go to page 2 PLEASE WORK</Link>
   </div>
 )
