3f961f00f1f21a3e425273b34880ea833bc11337
ANGELA MARTINEZ <angelaestmartinez@gmai.com>
2023-05-04 13:05:52 -0400

    Testing my commit message

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -5,7 +5,7 @@ const SecondPage = () => (
   <div>
     <h1>HELLO!!!</h1>
     <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Link to="/">Go back to the homepage NOW!</Link>
   </div>
 )
 
