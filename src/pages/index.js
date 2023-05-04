021ab729cc1fef7cf9afc1bbc558669e1dc85ca5
ANGELA MARTINEZ <angelaestmartinez@gmai.com>
2023-05-04 13:49:39 -0400

    1:49 testing for slack integration

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Learn to design and code React app</h1>
     <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-    <p>Testing for git commit 1:28pm</p>
+    <p>Testing for git commit 1:49PM testing for Slack integration</p>
     <Link to="/page-2/">Go to page 2 PLEASE WORK</Link>
   </div>
 )
